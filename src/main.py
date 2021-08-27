6cda36d6846895e080afd49eabd4da6222af8cd6
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2021-08-27 15:55:44 -0700

    update

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import sys
-sys.path.append("../OpenSeesPy_Building_Modeler")
+sys.path.append("OpenSeesPy_Building_Modeler")
 
 import numpy as np
 import modeler
 import solver
+
 
 # Define a building
 b = modeler.Building()
@@ -104,7 +105,7 @@ b.set_active_levels(['3'])
 b.set_active_section(b3lat)
 b.add_beams_from_grid_intersections(model_as=fiber_modeling_type)
 
-b.clear_gridlines()
+b.clear_gridlines_all()
 b.add_gridline('1', [32.5*12., 0.00], [107.5*12., 0.00])
 b.add_gridline('1b', [32.5*12., 0.00], [32.5*12., 1.00])
 b.add_gridline('1c', [57.5*12., 0.00], [57.5*12., 1.00])
@@ -140,7 +141,7 @@ b.set_active_levels(['3'])
 b.set_active_section(b3lat)
 b.add_beams_from_grid_intersections(model_as=fiber_modeling_type)
 
-b.clear_gridlines()
+b.clear_gridlines_all()
 b.add_gridline('A', [000.00*12., 000.00*12.], [000.00*12., 100.00*12.])
 b.add_gridline('B', [032.50*12., 000.00*12.], [032.50*12., 100.00*12.])
 b.add_gridline('C', [057.50*12., 000.00*12.], [057.50*12., 100.00*12.])
@@ -181,7 +182,7 @@ b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
 b.set_active_levels('all_above_base')
 b.set_active_section(bsecondary)
 
-b.clear_gridlines()
+b.clear_gridlines_all()
 b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
 b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
 b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
@@ -190,118 +191,51 @@ b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
 b.add_gridline('As1', [08.125*12., 000.00*12.], [08.125*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['As1'])
 b.add_gridline('As2', [016.25*12., 000.00*12.], [016.25*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['As2'])
 b.add_gridline('As3', [024.38*12., 000.00*12.], [024.38*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['As3'])
 b.add_gridline('Bs1', [040.83*12., 000.00*12.], [040.83*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Bs1'])
 b.add_gridline('Bs2', [049.17*12., 000.00*12.], [049.17*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Bs2'])
 b.add_gridline('Cs1', [065.83*12., 000.00*12.], [065.83*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Cs1'])
 b.add_gridline('Cs2', [074.17*12., 000.00*12.], [074.17*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Cs2'])
 b.add_gridline('Ds1', [090.83*12., 000.00*12.], [090.83*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Ds1'])
 b.add_gridline('Ds2', [099.17*12., 000.00*12.], [099.17*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Ds2'])
 b.add_gridline('Es1', [115.60*12., 000.00*12.], [115.60*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Es1'])
 b.add_gridline('Es2', [123.80*12., 000.00*12.], [123.80*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
-b.add_gridline('1', [000.00*12., 000.00*12.], [140.00*12., 000.00*12.])
-b.add_gridline('2', [000.00*12., 025.00*12.], [140.00*12., 025.00*12.])
-b.add_gridline('3', [000.00*12., 050.00*12.], [140.00*12., 050.00*12.])
-b.add_gridline('4', [000.00*12., 075.00*12.], [140.00*12., 075.00*12.])
-b.add_gridline('5', [000.00*12., 100.00*12.], [140.00*12., 100.00*12.])
+b.clear_gridlines(['Es2'])
 b.add_gridline('Es3', [131.90*12., 000.00*12.], [131.90*12., 100.00*12.])
 b.add_beams_from_grid_intersections(ends={'type': 'pinned', 'dist': 0.01},
                                     model_as=elastic_modeling_type)
-
-b.clear_gridlines()
+b.clear_gridlines_all()
 
 #
 # define surface loads
@@ -319,15 +253,15 @@ b.selection.add_UDL(np.array((0.00, 0.00, 0.00)))
 b.preprocess(assume_floor_slabs=True, self_weight=True)
 
 
-# b.plot_building_geometry(extrude_frames=True,
-#                          offsets=True,
-#                          gridlines=True,
-#                          global_axes=False,
-#                          diaphragm_lines=True,
-#                          tributary_areas=True,
-#                          just_selection=False,
-#                          parent_nodes=True,
-#                          frame_axes=False)
+b.plot_building_geometry(extrude_frames=True,
+                         offsets=True,
+                         gridlines=True,
+                         global_axes=False,
+                         diaphragm_lines=True,
+                         tributary_areas=True,
+                         just_selection=False,
+                         parent_nodes=True,
+                         frame_axes=False)
 
 
 # ~~~~~~~~~~~~~~~~~ #
@@ -354,20 +288,19 @@ b.preprocess(assume_floor_slabs=True, self_weight=True)
 #     print(node.coords)
 
 
-
 # ~~~~~~~~~~~~~~~~ #
 #  modal analysis  #
 # ~~~~~~~~~~~~~~~~ #
 
-# performing a linear modal analysis
-modal_analysis = solver.ModalAnalysis(b, num_modes=1)
+# # performing a linear modal analysis
+# modal_analysis = solver.ModalAnalysis(b, num_modes=1)
 
-import pdb
-pdb.set_trace()
-modal_analysis.run()
+# import pdb
+# pdb.set_trace()
+# modal_analysis.run()
 
-# retrieving textual results
-# print(modal_analysis.periods)
+# # retrieving textual results
+# # print(modal_analysis.periods)
 
-# visualizing results
-# modal_analysis.deformed_shape(step=0, scaling=0.00, extrude_frames=True)
+# # visualizing results
+# # modal_analysis.deformed_shape(step=0, scaling=0.00, extrude_frames=True)
