add4f9b15f8dd4b3ba095f5ccb1be326ce46cd89
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2022-02-28 10:57:44 -0800

    research meeting snapshot

--- src_experimental/performance_var_sens-edp.py
+++ src_experimental/performance_var_sens-edp.py
@@ -17,14 +17,11 @@ from p_58_assessment import P58_Assessment
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["mathtext.fontset"] = "dejavuserif"
 
-idx = pd.IndexSlice
-
 logging.basicConfig(
     filename=None,
     format='%(asctime)s %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S %p',
     level=logging.DEBUG)
-
 
 logging.info('Start')
 logging.info('Defining functions and global variables')
@@ -42,10 +39,20 @@ cmp_repair_cost_input_path = 'src_experimental/new_perf/input_repair_cost.csv'
 #                    Reading Input Data                          #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-asmt = P58_Assessment(num_realizations=1000)
-asmt.read_perf_model(perf_model_input_path)
-asmt.read_fragility_input(cmp_fragility_input_path)
-asmt.read_cmp_repair_cost_input(cmp_repair_cost_input_path)
+asmt_A = P58_Assessment(num_realizations=num_realizations)
+asmt_A.read_perf_model(perf_model_input_path)
+asmt_A.read_fragility_input(cmp_fragility_input_path)
+asmt_A.read_cmp_repair_cost_input(cmp_repair_cost_input_path)
+
+asmt_B = P58_Assessment(num_realizations=num_realizations)
+asmt_B.read_perf_model(perf_model_input_path)
+asmt_B.read_fragility_input(cmp_fragility_input_path)
+asmt_B.read_cmp_repair_cost_input(cmp_repair_cost_input_path)
+
+asmt_C = P58_Assessment(num_realizations=num_realizations)
+asmt_C.read_perf_model(perf_model_input_path)
+asmt_C.read_fragility_input(cmp_fragility_input_path)
+asmt_C.read_cmp_repair_cost_input(cmp_repair_cost_input_path)
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
@@ -53,137 +60,119 @@ asmt.read_cmp_repair_cost_input(cmp_repair_cost_input_path)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 logging.info('Starting analysis A')
-asmt.run(response_path, c_modeling_uncertainty)
+asmt_A.run(response_path, c_modeling_uncertainty)
 logging.info('\tAnalysis A finished')
 
 
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# #                       Analysis B                               #
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                       Analysis B                               #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-# logging.info('Starting analysis B')
-# analysis_B = {}
-# logging.info('\tGenerating simulated demands')
-# analysis_B['edp_RV'] = gen_edp_samples(response_path, num_realizations,
-#                                        c_modeling_uncertainty)
-# logging.info('\tSampling component quantities')
-# analysis_B['cmp_quant_RV_df'] = gen_cmp_quant_RV(perf_model_df)
-# logging.info('\tSampling component damage state thresholds')
-# analysis_B['cmp_fragility_RV_df'] = gen_cmp_fragility_RV(
-#     cmp_fragility_input_df, perf_model_df)
-# logging.info('\tDetermining component damage')
-# analysis_B['cmp_damage_df'] = calc_cmp_damage(
-#     perf_model_df, cmp_fragility_input_df,
-#     analysis_B['edp_RV'],
-#     analysis_B['cmp_fragility_RV_df'])
-# logging.info('\tDetermining damage consequences')
-# analysis_B['cmp_damage_consequence_RV_df'] = gen_cmp_damage_consequence_RV(
-#     perf_model_df, cmp_repair_cost_input_df)
-# analysis_B['cmp_dmg_quant_df'], analysis_B['cmp_dmg_quant_eco_df'] = \
-#     calc_cmp_dmg_quant(
-#         perf_model_df, cmp_repair_cost_input_df,
-#         analysis_B['cmp_damage_df'],
-#         analysis_B['cmp_damage_consequence_RV_df'],
-#         analysis_B['cmp_quant_RV_df'])
-# logging.info('\tDetermining component repair cost')
-# analysis_B['cmp_cost_RV_df'] = gen_cmp_cost_RV(
-#     analysis_B['cmp_dmg_quant_df'])
-# analysis_B['cmp_cost_df'] = calc_cmp_cost(
-#     analysis_B['cmp_dmg_quant_df'],
-#     analysis_B['cmp_cost_RV_df'],
-#     cmp_repair_cost_input_df,
-#     analysis_B['cmp_dmg_quant_eco_df'])
-# logging.info('\tSummarizing cost')
-# analysis_B['total_cost_df'] = calc_total_cost(
-#     analysis_B['cmp_cost_df'],
-#     cmp_repair_cost_input_df,
-#     analysis_B['cmp_cost_RV_df'])
-# logging.info('\tAnalysis B finished')
+logging.info('Starting analysis B')
+asmt_B.run(response_path, c_modeling_uncertainty)
+logging.info('\tAnalysis A finished')
 
 
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# #                       Analysis C                               #
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                       Analysis C                               #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+# EDP
 # logging.info('Starting analysis C')
-# analysis_C = {}
-# logging.info('\tUsing simulated demands from A')
-# analysis_C['edp_RV'] = analysis_A['edp_RV']
-# logging.info('\tUsing component quantities from B')
-# analysis_C['cmp_quant_RV_df'] = analysis_B['cmp_quant_RV_df']
-# logging.info('\tUsing component damage state thresholds from B')
-# analysis_C['cmp_fragility_RV_df'] = analysis_B['cmp_fragility_RV_df']
-# logging.info('\tDetermining component damage')
-# analysis_C['cmp_damage_df'] = calc_cmp_damage(
-#     perf_model_df, cmp_fragility_input_df,
-#     analysis_C['edp_RV'],
-#     analysis_C['cmp_fragility_RV_df'])
-# logging.info('\tDetermining damage consequences')
-# analysis_C['cmp_damage_consequence_RV_df'] = \
-#     analysis_B['cmp_damage_consequence_RV_df']
-# analysis_C['cmp_dmg_quant_df'], analysis_C['cmp_dmg_quant_eco_df'] = \
-#     calc_cmp_dmg_quant(
-#         perf_model_df, cmp_repair_cost_input_df,
-#         analysis_C['cmp_damage_df'],
-#         analysis_C['cmp_damage_consequence_RV_df'],
-#         analysis_C['cmp_quant_RV_df'])
-# logging.info('\tDetermining component repair cost')
-# analysis_C['cmp_cost_RV_df'] = analysis_B['cmp_cost_RV_df']
-# analysis_C['cmp_cost_df'] = calc_cmp_cost(
-#     analysis_C['cmp_dmg_quant_df'],
-#     analysis_C['cmp_cost_RV_df'],
-#     cmp_repair_cost_input_df,
-#     analysis_C['cmp_dmg_quant_eco_df'])
-# logging.info('\tSummarizing cost')
-# analysis_C['total_cost_df'] = calc_total_cost(
-#     analysis_C['cmp_cost_df'],
-#     cmp_repair_cost_input_df,
-#     analysis_C['cmp_cost_RV_df'])
+# asmt_C.edp_samples = asmt_A.edp_samples
+# asmt_C.cmp_quant_RV = asmt_B.cmp_quant_RV
+# asmt_C.cmp_fragility_RV = asmt_B.cmp_fragility_RV
+# asmt_C.cmp_damage_consequence_RV = asmt_B.cmp_damage_consequence_RV
+# asmt_C.cmp_cost_RV = asmt_B.cmp_cost_RV
+# asmt_C.calc_cmp_damage()
+# asmt_C.calc_cmp_dmg_quant()
+# asmt_C.calc_cmp_cost()
+# asmt_C.calc_total_cost()
 # logging.info('\tAnalysis C finished')
 
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# #                     Sensitivity Indices                        #
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-# logging.info('Calculating sensitivity indices for the EDPs')
-# yA = analysis_A['total_cost_df'].to_numpy()
-# yB = analysis_B['total_cost_df'].to_numpy()
-# yC = analysis_C['total_cost_df'].to_numpy()
-
-
-# def calc_sens(yA, yB, yC):
-#     n = len(yA)
-#     f0 = 1/n * np.sum(yA)
-#     s1 = (1./n * (yA.T @ yC) - f0**2) / (1./n * (yA.T @ yA) - f0**2)
-#     sT = 1. - (1/n * (yB.T @ yC) - f0**2) / \
-#         (1/n * (yA.T @ yA) - f0**2)
-#     return s1, sT
-
-
-# s1, sT = calc_sens(yA, yB, yC)
-# print()
-# print("EDPs")
-# print(f"1st order sensitivity index: {s1:.3f}")
-# print(f"Total sensitivity index    : {sT:.3f}")
+# component repair cost (cmp DV)
+# logging.info('Starting analysis C')
+# asmt_C.edp_samples = asmt_B.edp_samples
+# asmt_C.cmp_quant_RV = asmt_B.cmp_quant_RV
+# asmt_C.cmp_fragility_RV = asmt_B.cmp_fragility_RV
+# asmt_C.calc_cmp_damage()
+# asmt_C.cmp_damage_consequence_RV = asmt_B.cmp_damage_consequence_RV
+# asmt_C.cmp_cost_RV = asmt_B.cmp_cost_RV
+# asmt_C.calc_cmp_dmg_quant()
+# temp = asmt_A.cmp_cost_RV
+# temp.loc[:, ('replacement', '0', '1', '0', 'DS1', '1')] = \
+#     asmt_B.cmp_cost_RV.loc[:, ('replacement', '0', '1', '0', 'DS1', '1')]
+# asmt_C.cmp_cost_RV = temp
+# asmt_C.calc_cmp_cost()
+# asmt_C.calc_total_cost()
+# logging.info('\tAnalysis C finished')
 
 
+# component fragilities
+logging.info('Starting analysis C')
+asmt_C.edp_samples = asmt_B.edp_samples
+asmt_C.cmp_quant_RV = asmt_B.cmp_quant_RV
+temp = asmt_A.cmp_fragility_RV
+subset_cols = [('collapse', '0', '1', '0', 'DS1'),
+               ('irreparable', '0', '1', '0', 'DS1')]
+temp.loc[:, subset_cols] = \
+    asmt_B.cmp_fragility_RV.loc[:, subset_cols]
+asmt_C.cmp_fragility_RV = temp
+asmt_C.calc_cmp_damage()
+asmt_C.cmp_damage_consequence_RV = asmt_A.cmp_damage_consequence_RV
+asmt_C.calc_cmp_dmg_quant()
+asmt_C.cmp_cost_RV = asmt_B.cmp_cost_RV
+asmt_C.calc_cmp_cost()
+asmt_C.calc_total_cost()
+logging.info('\tAnalysis C finished')
 
-# num_repeats = 10000
-# sel = np.random.choice(num_realizations, (num_repeats, num_realizations))
-# bootstrap_sample = np.zeros(num_repeats)
-# for j in range(num_repeats):
-#     bootstrap_sample[j] = calc_sens(yA[sel[j]], yB[sel[j]], yC[sel[j]])[0]
 
 
-# from scipy import stats
-# plt.hist(bootstrap_sample, 50, density=True, color='lightgrey', edgecolor='black')
-# xv = np.linspace(0.60, 0.70, 10000)
-# mub = bootstrap_sample.mean()
-# mus = bootstrap_sample.std()
-# yv = stats.norm.pdf(xv, loc=mub, scale=mus)
-# plt.plot(xv, yv)
-# plt.show()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                     Sensitivity Indices                        #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+logging.info('Calculating sensitivity indices for the EDPs')
+yA = asmt_A.total_cost.to_numpy()
+yB = asmt_B.total_cost.to_numpy()
+yC = asmt_C.total_cost.to_numpy()
+
+
+def calc_sens(yA, yB, yC):
+    n = len(yA)
+    f0 = 1/n * np.sum(yA)
+    s1 = (1./n * (yA.T @ yC) - f0**2) / (1./n * (yA.T @ yA) - f0**2)
+    sT = 1. - (1/n * (yB.T @ yC) - f0**2) / \
+        (1/n * (yA.T @ yA) - f0**2)
+    return s1, sT
+
+
+s1, sT = calc_sens(yA, yB, yC)
+print()
+print("EDPs")
+print(f"1st order sensitivity index: {s1:.3f}")
+print(f"Total sensitivity index    : {sT:.3f}")
+
+
+num_repeats = 10000
+sel = np.random.choice(num_realizations, (num_repeats, num_realizations))
+bootstrap_sample = np.zeros(num_repeats)
+for j in range(num_repeats):
+    bootstrap_sample[j] = calc_sens(yA[sel[j]], yB[sel[j]], yC[sel[j]])[0]
+
+
+from scipy import stats
+fig = plt.figure(figsize=(6, 6))
+plt.hist(bootstrap_sample, 50, density=True, color='lightgrey', edgecolor='black')
+xv = np.linspace(np.min(bootstrap_sample), np.max(bootstrap_sample), 10000)
+mub = bootstrap_sample.mean()
+mus = bootstrap_sample.std()
+yv = stats.norm.pdf(xv, loc=mub, scale=mus)
+plt.plot(xv, yv)
+plt.savefig('tmp_results/si1_cmp_DM_hz3.pdf')
+plt.show()
+plt.close()
 
 
 
